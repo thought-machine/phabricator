c62a04dbfde7c867432fb3619ef6bba1f3dffcd0
Chris Novakovic <chris@chrisn.me.uk>
2020-10-10 16:28:13 +0100

    Revert "Add handler for git upload-archive to Diffusion"
    The handler in this commit doesn't work correctly: git-upload-archive
    doesn't do full command line option parsing (argv[1] is simply treated
    as the repository path), so passing "-- <repo_path>" as arguments to
    git-upload-archive in the call to sudoCommandAsDaemonUser() results in
    
        0008ACK
        00000026usage: git upload-archive <repo>
    
    being printed to stdout. The solution is simply to pass "<repo_path>" as
    the sole argument.
    
    This reverts commit 5ecea584edfdacaa12b0f4bdf52046aa2a8b9557.

--- src/__phutil_library_map__.php
+++ src/__phutil_library_map__.php
@@ -9,7 +9,6 @@
 phutil_register_library_map(array(
   '__library_version__' => 2,
   'class' => array(
-    'DiffusionGitUploadArchiveSSHWorkflow' => 'applications/diffusion/ssh/DiffusionGitUploadArchiveSSHWorkflow.php',
     'PhabricatorAphlictManagementForegroundWorkflow' => 'applications/aphlict/management/PhabricatorAphlictManagementForegroundWorkflow.php',
     'PhabricatorAuthProvider' => 'applications/auth/provider/PhabricatorAuthProvider.php',
     'PhabricatorAuthStartController' => 'applications/auth/controller/PhabricatorAuthStartController.php',
@@ -21,7 +20,6 @@ phutil_register_library_map(array(
   ),
   'function' => array(),
   'xmap' => array(
-    'DiffusionGitUploadArchiveSSHWorkflow' => 'DiffusionGitSSHWorkflow',
     'PhabricatorAphlictManagementForegroundWorkflow' => 'PhabricatorAphlictManagementWorkflow',
     'PhabricatorAuthProvider' => 'Phobject',
     'PhabricatorAuthStartController' => 'PhabricatorAuthController',
