8b157cf8c0957023dd39a41327389fee4ced9076
rgodden <rgodden@thoughtmachine.net>
2021-03-05 21:09:05 +0000

    setting correct source for safeland

--- src/applications/differential/controller/DifferentialRevisionViewController.php
+++ src/applications/differential/controller/DifferentialRevisionViewController.php
@@ -818,7 +818,7 @@ final class DifferentialRevisionViewController
 
       $curtain->addAction($action);
       //  TM CHANGES BEGIN: including safe land indicator
-      require_celerity_resource('safe-landing');
+      require_celerity_resource('safe-landing', 'tmphabricator');
       // TM CHANGES END
     }
 
