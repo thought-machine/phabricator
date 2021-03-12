dab9db7267406160c2613502f9cd81da03b18afe
Richard Godden <7768980+goddenrich@users.noreply.github.com>
2021-03-12 10:24:28 +0000

    typo

--- src/applications/prometheus/metrics/PhabricatorPrometheusMetricGauge.php
+++ src/applications/prometheus/metrics/PhabricatorPrometheusMetricGauge.php
@@ -5,7 +5,7 @@ use Prometheus\CollectorRegistry;
 /**
  * @phutil-external-symbol class CollectorRegistry
  */
-abstract class PhabricatorPrometheusMetricGauge extends PhabricatorPrometueusMetric {
+abstract class PhabricatorPrometheusMetricGauge extends PhabricatorPrometheusMetric {
 
   final private function observe(float $value, array $labels): void {
     $metric->set($value, $labels);
