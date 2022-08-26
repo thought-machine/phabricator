0d62919a9ae19a172c9d0589b847d5deb135346b
rgodden <rgodden@thoughtmachine.net>
2022-08-26 15:32:29 +0100

    adding herald rule and harbormaster for refs like tags and branches

---
+++ src/applications/diffusion/herald/HeraldPreCommitRefAdapter.php
@@ -0,0 +1,54 @@
+<?php
+
+final class HeraldPreCommitRefAdapter
+  extends HeraldPreCommitAdapter 
+  implements HarbormasterBuildableAdapterInterface {
+
+  private $buildRequests = array();
+
+  public function getAdapterContentName() {
+    return pht('Commit Hook: Branches/Tags/Bookmarks');
+  }
+
+  public function getAdapterSortOrder() {
+    return 2000;
+  }
+
+  public function getAdapterContentDescription() {
+    return pht(
+      "React to branches and tags being pushed to hosted repositories.\n".
+      "Hook rules can block changes and send push summary mail.");
+  }
+
+  public function isPreCommitRefAdapter() {
+    return true;
+  }
+
+  public function getHeraldName() {
+    return pht('Push Log (Ref)');
+  }
+
+
+  /* TM CHANGES */
+  /* -(  HarbormasterBuildableAdapterInterface  )------------------------------ */
+
+
+  public function getHarbormasterBuildablePHID() {
+    return $this->getObject()->getPHID();
+  }
+
+  public function getHarbormasterContainerPHID() {
+    return $this->getObject()->getRepository()->getPHID();
+  }
+
+  public function getQueuedHarbormasterBuildRequests() {
+    return $this->buildRequests;
+  }
+
+  public function queueHarbormasterBuildRequest(
+    HarbormasterBuildRequest $request) {
+    $this->buildRequests[] = $request;
+  }
+
+  /* TM CHANGES END */
+}
