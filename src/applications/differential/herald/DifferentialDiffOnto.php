2adde582c61d15addf2158954bb039084c0ea07f
Richard Godden <7768980+goddenrich@users.noreply.github.com>
2024-03-14 12:42:23 +0000

    adding onto branch as herald filed (#78)
    This allows us to add herald rules for diffs based on the target land onto branch.

---
+++ src/applications/differential/herald/DifferentialDiffOnto.php
@@ -0,0 +1,24 @@
+i<?php
+
+final class DifferentialDiffOntoField
+  extends DifferentialDiffHeraldField {
+
+  const FIELDCONST = 'differential.diff.refs.onto';
+
+  public function getHeraldFieldName() {
+    return pht('Onto branch');
+  }
+
+  public function getHeraldFieldValue($object) {
+    $onto = $this->loadTargetBranch();
+    if (phutil_nonempty_string($onto)) {
+      return $onto;
+    }
+    return '';
+  }
+
+  protected function getHeraldFieldStandardType() {
+    return self::STANDARD_TEXT;
+  }
+
+}
