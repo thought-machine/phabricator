a763423f7738a287c0c745ff555a61d4f9e6bd6a
Chris Novakovic <chris@chrisn.me.uk>
2025-07-11 16:08:16 +0100

    Expose a diff's lint status via `differential.diff.search`
    Permit users to optionally request that `differential.diff.search`
    return the lint status of each diff in the result set as an attachment.
    
    This is a more efficient way to query a diff's lint status than via
    `differential.querydiffs`, which returns everything Differential knows
    about a diff (including its change sets) and is therefore very slow -
    requests to `differential.querydiffs` will often time out if
    `revisionIDs` contains more than five revisions of moderate complexity.
    
    Implements https://we.phorge.it/T16152.

---
+++ src/applications/differential/engineextension/DifferentialLintSearchEngineAttachment.php
@@ -0,0 +1,26 @@
+<?php
+
+final class DifferentialLintSearchEngineAttachment
+  extends PhabricatorSearchEngineAttachment {
+
+  public function getAttachmentName() {
+    return pht('Lint');
+  }
+
+  public function getAttachmentDescription() {
+    return pht('Get the lint status of each diff.');
+  }
+
+  public function getAttachmentForObject($object, $data, $spec) {
+    $status_value = $object->getLintStatus();
+    $status = DifferentialLintStatus::newStatusFromValue($status_value);
+
+    return array(
+      'status' => array(
+        'value' => $status->getValue(),
+        'name' => $status->getName(),
+      ),
+    );
+  }
+
+}
